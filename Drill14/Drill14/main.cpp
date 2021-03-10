42b85919cdf9b9e51630d8a84570d6b71c243456
ImSamhel <shay9505@gmail.com>
2021-03-10 16:02:56 +0100

    Drill 14 minor changes

--- Drill14/Drill14/main.cpp
+++ Drill14/Drill14/main.cpp
@@ -69,16 +69,19 @@ int main() {
 	B1 objectB1;
 	objectB1.vf();
 	objectB1.f();
+	objectB1.pvf();
 
 	std::cout << "D1:B1 Object" << std::endl;
 	D1 objectD1;
 	objectD1.vf();
 	objectD1.f(); // nincs felülirva de hivhatjuk a B1 public functionjei közül
+	objectD1.pvf();
 
 	std::cout << "B1 Reference" << std::endl;
 	B1& referenceB1 = objectD1;
 	referenceB1.vf();
 	referenceB1.f();
+	referenceB1.pvf();
 
 	std::cout << std::endl;
 	//Repeat
@@ -87,16 +90,19 @@ int main() {
 	B1 objectB1_2;
 	objectB1_2.vf();
 	objectB1_2.f();
+	objectB1_2.pvf();
 
 	std::cout << "D1:B1 2. Object" << std::endl;
 	D1 objectD1_2;
 	objectD1_2.vf();
 	objectD1_2.f(); // nincs felülirva de megírtuk a D1 saját f() functionjét
+	objectD1_2.pvf();
 
 	std::cout << "B1 2. Reference" << std::endl;
 	B1& referenceB1_2 = objectD1_2;
 	referenceB1_2.vf();
 	referenceB1_2.f();
+	referenceB1_2.pvf();
 
 	std::cout << std::endl;
 	//D2
