013d16906fcded4500a90e312130c8deb8738dd5
ImSamhel <shay9505@gmail.com>
2021-04-18 16:11:22 +0200

    drill19
    <3

---
+++ Drill19/drill19/drill19/main.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include "std_lib_facilities.h"
+
+template<typename T> struct S {
+private:
+	T val;
+public:
+	S(T a) { val = a; }
+	T& get() const;
+	T& get();
+	void operator=(const T&);
+};
+
+template<typename T> T& S<T>::get() const {
+	return S::val;
+}
+
+template<typename T> T& S<T>::get() {
+	return S::val;
+}
+
+template<typename T> void S<T>::operator=(const T& t) {
+	S::val = t.get();
+}
+
+template<typename T> void read_val(T& v) {
+	T t;
+	std::cin >> t;
+	v = t;
+}
+int main() {
+	vector<int> vekivek{ 1, 2, 3 };
+
+	S<int> Int(2);
+	S<char> Char('c');
+	S<double> Double(2.69);
+	S<std::string> String("besencz");
+	S<vector<int>> Vector(vekivek);
+
+	std::cout << (Int.get()) << std::endl;
+	std::cout << (Char.get()) << std::endl;
+	std::cout << (Double.get()) << std::endl;
+	std::cout << (String.get()) << std::endl;
+	for (int k : Vector.get()) std::cout << k << std::endl;
+
+	read_val(Int.get());
+	read_val(Char.get());
+	read_val(Double.get());
+	read_val(String.get());
+
+	std::cout << (Int.get()) << std::endl;
+	std::cout << (Char.get()) << std::endl;
+	std::cout << (Double.get()) << std::endl;
+	std::cout << (String.get()) << std::endl;
+
+	return 0;
+}
